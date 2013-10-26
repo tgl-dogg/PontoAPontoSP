b86b77d5d61356d711e345f3415709dde48c342d
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 12:00:26 -0200

    Adicionado Construtor, Getters e Setters para classe Agency

--- ProntoAPontoSP/src/inovapap/sp/gtfs/Agency.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/Agency.java
@@ -1,9 +1,72 @@
 package inovapap.sp.gtfs;
 
+import inovapap.sp.util.Parser;
+
 public class Agency {
 	private String agencyId;
 	private String agencyName;
 	private String agencyUrl;
 	private String agencyTimezone;
 	private String agencyLang;
+
+	public Agency(String line){
+		String txt = line;
+		Parser parse = new Parser();
+		
+		this.agencyId = parse.stringParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.agencyName = parse.stringParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.agencyUrl = parse.stringParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.agencyTimezone = parse.stringParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.agencyLang = parse.stringParse(txt);
+		txt = parse.removeComma(txt);
+	}
+	
+	public String getAgencyId() {
+		return agencyId;
+	}
+
+	public void setAgencyId(String agencyId) {
+		this.agencyId = agencyId;
+	}
+
+	public String getAgencyName() {
+		return agencyName;
+	}
+
+	public void setAgencyName(String agencyName) {
+		this.agencyName = agencyName;
+	}
+
+	public String getAgencyUrl() {
+		return agencyUrl;
+	}
+
+	public void setAgencyUrl(String agencyUrl) {
+		this.agencyUrl = agencyUrl;
+	}
+
+	public String getAgencyTimezone() {
+		return agencyTimezone;
+	}
+
+	public void setAgencyTimezone(String agencyTimezone) {
+		this.agencyTimezone = agencyTimezone;
+	}
+
+	public String getAgencyLang() {
+		return agencyLang;
+	}
+
+	public void setAgencyLang(String agencyLang) {
+		this.agencyLang = agencyLang;
+	}
+
 }
