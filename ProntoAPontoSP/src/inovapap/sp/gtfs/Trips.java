a74fcc2684d19ac48a4844019f0c93a4e3c0ec98
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 12:43:40 -0200

    Adicionados atributos para a classe Trips

--- ProntoAPontoSP/src/inovapap/sp/gtfs/Trips.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/Trips.java
@@ -1,5 +1,11 @@
 package inovapap.sp.gtfs;
 
 public class Trips {
+	private String routeId;
+	private String serviceId;
+	private String tripId;
+	private String tripHeadsign;
+	private int directionId;
+	private int shapeId;
 
 }
