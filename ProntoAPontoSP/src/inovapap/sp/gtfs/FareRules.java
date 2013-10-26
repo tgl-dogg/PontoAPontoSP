2c9fc3f289e1c03b0e0cee3dd90e3aa7f96fb888
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 12:18:29 -0200

    Adicionado atributos para a classe FareRules

--- ProntoAPontoSP/src/inovapap/sp/gtfs/FareRules.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/FareRules.java
@@ -1,5 +1,9 @@
 package inovapap.sp.gtfs;
 
 public class FareRules {
-
+	private String fareId;
+	private String routeId;
+	private String originId;
+	private String destinationId;
+	private String containsId;
 }
