e346c5e448b1aad60b8e024d9ae4e0f24e346158
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 12:39:07 -0200

    Adicionados atributos para a classe StopTimes

--- ProntoAPontoSP/src/inovapap/sp/gtfs/StopTimes.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/StopTimes.java
@@ -1,5 +1,9 @@
 package inovapap.sp.gtfs;
 
 public class StopTimes {
-
+	private String tripId;
+	private String arrivalTime;
+	private String departureTime;
+	private int stopId;
+	private int stopSequence;
 }
