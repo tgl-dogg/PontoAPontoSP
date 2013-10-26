b7d680839e6d03ed734c1f70ee25b64e5b107e27
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 12:10:02 -0200

    Adicionado atributos da classe FareAttributes

--- ProntoAPontoSP/src/inovapap/sp/gtfs/FareAttributes.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/FareAttributes.java
@@ -1,5 +1,10 @@
 package inovapap.sp.gtfs;
 
 public class FareAttributes {
-
+	private String fareId;
+	private float price;
+	private String currencyType;
+	private String paymentMethod;
+	private int transfers;
+	private int transfer_duration;
 }
