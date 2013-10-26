c25fb9d0ac18a16379361098bbfb97db0318ff22
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 12:16:28 -0200

    Adicionado Construtor, Getters e Setters para a classe FareAttributes

--- ProntoAPontoSP/src/inovapap/sp/gtfs/FareAttributes.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/FareAttributes.java
@@ -1,5 +1,7 @@
 package inovapap.sp.gtfs;
 
+import inovapap.sp.util.Parser;
+
 public class FareAttributes {
 	private String fareId;
 	private float price;
@@ -7,4 +9,74 @@ public class FareAttributes {
 	private String paymentMethod;
 	private int transfers;
 	private int transfer_duration;
+	
+	public FareAttributes(String line){
+		String txt = line;
+		Parser parse = new Parser();
+		
+		this.fareId = parse.stringParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.price = parse.floatParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.currencyType = parse.stringParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.paymentMethod = parse.stringParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.transfers = parse.intParse(txt);
+		txt = parse.removeComma(txt);
+		
+		this.transfer_duration = parse.intParse(txt);		
+	}
+
+	public String getFareId() {
+		return fareId;
+	}
+
+	public void setFareId(String fareId) {
+		this.fareId = fareId;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getCurrencyType() {
+		return currencyType;
+	}
+
+	public void setCurrencyType(String currencyType) {
+		this.currencyType = currencyType;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public int getTransfers() {
+		return transfers;
+	}
+
+	public void setTransfers(int transfers) {
+		this.transfers = transfers;
+	}
+
+	public int getTransfer_duration() {
+		return transfer_duration;
+	}
+
+	public void setTransfer_duration(int transfer_duration) {
+		this.transfer_duration = transfer_duration;
+	}
 }
