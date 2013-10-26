94173ec80e3d8f2467eeeefa4135916d78932ea0
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 12:04:15 -0200

    Adicionado atributos da classe Calendar

--- ProntoAPontoSP/src/inovapap/sp/gtfs/Calendar.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/Calendar.java
@@ -1,5 +1,14 @@
 package inovapap.sp.gtfs;
 
 public class Calendar {
-
+	private String serviceId;
+	private int monday;
+	private int tuesday;
+	private int wednesday;
+	private int thursday;
+	private int friday;
+	private int saturday;
+	private int sunday;
+	private String start_date;
+	private String end_date;
 }
