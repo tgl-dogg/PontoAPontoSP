1d54cd2b76ac8229c4f340314592967f25cd4296
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 12:32:51 -0200

    Adicionado atributos da classe Shapes

--- ProntoAPontoSP/src/inovapap/sp/gtfs/Shapes.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/Shapes.java
@@ -1,5 +1,9 @@
 package inovapap.sp.gtfs;
 
 public class Shapes {
-
+	private int shapeId;
+	private float shapePtLat;
+	private float shapePtLon;
+	private int shapePtSequence;
+	private float shapeDistTraveled;
 }
