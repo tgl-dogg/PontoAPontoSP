17ff552d7f5ea0eb08e9a7c4bcdcacc07ca34281
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 14:48:41 -0200

    PontoAPontoActivity preparada para receber mapa

--- ProntoAPontoSP/src/inovapap/sp/PontoAPontoActivity.java
+++ ProntoAPontoSP/src/inovapap/sp/PontoAPontoActivity.java
@@ -1,7 +1,15 @@
 package inovapap.sp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import inovapap.sp.gtfs.Stops;
+import inovapap.sp.util.Geral;
 import inovapap.sp.util.ILog;
+import inovapap.sp.util.Parser;
 import android.content.pm.ActivityInfo;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -13,11 +21,14 @@ public class PontoAPontoActivity extends FragmentActivity {
 		try {
 			super.onCreate(b);
 			
+//			InputStream is = getResources().openRawResource(R.raw.stops);
+//			Parser parser = new Parser();
+//			String line = parser.generalParseLine(is);
+//			Stops stop = new Stops(line);
 			
 			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		} catch (Exception ex) {
 			ILog.e(TAG + "onCreate()", ex.getMessage());
 		}
 	}
-
 }
