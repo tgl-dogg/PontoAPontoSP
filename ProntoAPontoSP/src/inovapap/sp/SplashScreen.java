adca031c92c96a021d7a2e3ab7bba51447ac23f9
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 14:00:43 -0200

    Adicionado o código para SplashScreen

--- ProntoAPontoSP/src/inovapap/sp/SplashScreen.java
+++ ProntoAPontoSP/src/inovapap/sp/SplashScreen.java
@@ -1,14 +1,32 @@
 package inovapap.sp;
 
+import inovapap.sp.util.ILog;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashScreen extends Activity {
+public class SplashScreen extends Activity implements Runnable {
+	private final String TAG = "SplashScreen ";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash_screen);
+		try {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_splash_screen);
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+			Handler handler = new Handler();
+			handler.postDelayed(this, 3000);
+		} catch (Exception ex) {
+			ILog.e(TAG + "onCreate()", ex.getMessage());
+		}
 	}
 
+	@Override
+	public void run() {
+		startActivity(new Intent(this, PontoAPontoActivity.class));
+		finish();
+	}
 }
