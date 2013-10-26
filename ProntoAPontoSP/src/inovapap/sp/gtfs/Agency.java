edf35ae12f3ce3d50a8ab645c2e8d88558734ffd
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 11:53:10 -0200

    Adicionado campos da classe Agency

--- ProntoAPontoSP/src/inovapap/sp/gtfs/Agency.java
+++ ProntoAPontoSP/src/inovapap/sp/gtfs/Agency.java
@@ -1,5 +1,9 @@
 package inovapap.sp.gtfs;
 
 public class Agency {
-
+	private String agencyId;
+	private String agencyName;
+	private String agencyUrl;
+	private String agencyTimezone;
+	private String agencyLang;
 }
