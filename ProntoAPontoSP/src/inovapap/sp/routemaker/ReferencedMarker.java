497ac6daf122a3f6b7ed5e5bb58bb3cbc9fe37c4
Vinicius de Carvalho <vinidoggll@hotmail.com>
2013-10-26 23:23:54 -0200

    Criação do ReferencedMarker para tratamento dos pontos na tela do mapa

---
+++ ProntoAPontoSP/src/inovapap/sp/routemaker/ReferencedMarker.java
@@ -0,0 +1,26 @@
+package inovapap.sp.routemaker;
+
+import com.google.android.gms.maps.model.Marker;
+
+public class ReferencedMarker {
+
+	Marker marker;
+	int id;
+
+	public void add(Marker mk, int i) {
+		marker = mk;
+		id = i;
+	}
+	
+	public boolean find(Marker mk){
+		return mk.equals(marker);
+	}
+	
+	public void addMarker(Marker mk){
+		this.marker = mk;
+	}
+	
+	public int getId(){
+		return id;
+	}
+}
